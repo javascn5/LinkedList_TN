58f368ea493f7f83a2c8db4a2dce2cb688532554
tornadusNEWB <nowaczek5@o2.pl>
2018-10-21 14:34:01 +0200

    new linkedList

--- src/com/sda/kolekcje/CustomLinkedList.java
+++ src/com/sda/kolekcje/CustomLinkedList.java
@@ -1,6 +1,15 @@
 package com.sda.kolekcje;
 
-public class CustomLinkedList <E> implements CustomList <E> {
+public class CustomLinkedList<E> implements CustomList<E> {
+    // towrzymy 2 pola
+    private int size;
+    // head- poczatek listy
+    private Node head;
+
+    public CustomLinkedList() {
+        head = new Node(null);
+    }
+
 
     @Override
     public E get(int index) {
@@ -10,10 +19,48 @@ public class CustomLinkedList <E> implements CustomList <E> {
     @Override
     public void add(E e) {
 
+        // kopiujemy liste ktora istnieje dotychczas
+
+        Node copy= head;
+        System.out.println("wartosc copy: " + copy);
+        head = new Node(e);
+        System.out.println("wartosc head: " + head);
+        head.next = copy;
+        System.out.println("wartosc next: " + head.next);
+        size++;
+
+
     }
 
     @Override
     public int size() {
         return 0;
+    }
+    // kazdy obiekt listy musi byc nowym obiektem to robimy nowa klase Node
+    // ona ma 2 referencje- data i wskaznik
+    // bedzie odpowiedzialna za przechowanie
+
+    class Node {
+        private Object data;
+        private Node next;
+
+        // konstruktor
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        // getter
+        public Object getData() {
+            return data;
+        }
+
+        // metoda toString - zwraca nam aktualne wartosci
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
+        }
     }
 }
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
         // linkedList - implementacja listy, ktora bazuje na obiektach
         // kazdy element listy wskazuje na element poprzedni i nastepny
 
@@ -13,16 +14,22 @@ public class Main {
         // LinkedList jest implementacja wiazana- kiedy duzo usuwamy danych
 
         List<String> list = new LinkedList<>();
-
-
         list.add("Jan");
         list.add("Pawel");
         list.add("Gawel");
-
-
-        for (String lista: list){
+        for (String lista : list) {
             System.out.println(lista);
         }
 
+
+        CustomList<String> customList = new CustomLinkedList<>();
+
+        customList.add("jeden");
+        customList.add("dwa");
+        customList.add("trzy");
+        customList.add("cztery");
+
+// printujac to wynnika z tego, ze
+        // trzeci obiekt rzutuje na drugi, drugi na pierwszy, pierwszy na nulla
     }
 }
