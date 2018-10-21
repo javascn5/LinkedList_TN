5be75ecb41a193051d388d7a2c66d945ec9a727d
tornadusNEWB <nowaczek5@o2.pl>
2018-10-21 13:38:09 +0200

    prepare LinkedlistImplementation

---
+++ src/com/sda/kolekcje/CustomList.java
@@ -0,0 +1,8 @@
+package com.sda.kolekcje;
+
+public interface CustomList <E> {
+    // towrzymy metode get, add , size
+    public E get (int index);
+    public void add (E e);
+    public int size();
+}
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,28 @@
 package com.sda.kolekcje;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // linkedList - implementacja listy, ktora bazuje na obiektach
+        // kazdy element listy wskazuje na element poprzedni i nastepny
+
+        // ArrayList jest implementacja tablicowa- szybsza
+        // LinkedList jest implementacja wiazana- kiedy duzo usuwamy danych
+
+        List<String> list = new LinkedList<>();
+
+
+        list.add("Jan");
+        list.add("Pawel");
+        list.add("Gawel");
+
+
+        for (String lista: list){
+            System.out.println(lista);
+        }
+
     }
 }
